7056a845d4470ee8cb030b29abc8b128bb326fdb
Tasha R Zuniga <Tashazun@gmail.com>
2018-02-06 13:42:31 -0800

    Finished all objects and functions

--- app.js
+++ app.js
@@ -1,160 +1,204 @@
 'use strict'
+// Store hour array, and total of cookies sold for the day.
+const storeHours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ','6pm: ', '7pm: ',  '8pm: ', 'Total: '];
+
 // Amount of cookies needed at the PDX Airport
 const airport = {
     minHourly: 23,
     maxHourly: 65,
     cookiePer: 6.3,
-    total: [],
+    totalCookies: [],
     customerGen: function() {
-        return Math.floor(this.minHourly) + Math.floor(Math.random() * 65) + 1;
+        return Math.floor(Math.random() * (this.maxHourly - this.minHourly)) + this.minHourly;
     },
     customerPurchase: function() {
-        return Math.floor(Math.random() * 6.3) + 1 + this.customerGen();
+        return Math.floor(Math.random() * this.cookiePer) + 1 + this.customerGen();
     },
-    totalCookie: function() {
+    cookieBought: function() {
         for (let i = 0; i < 15; i++) { 
-            this.total.push(this.customerPurchase());
+            this.totalCookies.push(this.customerPurchase());
             }
         },
-    addList: function() {
-        for (let x = 0; x < 15; x++) {       
-            const list = document.getElementById('cookieList');
-            const li = document.createElement('li');
-        } if (x < 4) {  
-            li.textContent = x + ' am: ' + this.total[x] + ' cookies';
-            list.appendChild(li);
-        } else if (x < 4) {
-            li.textContent = x + ' pm: ' + this.total[x] + ' cookies';
-            list.appendChild(li)
+    cookieTotal: function() {
+        let tally = 0
+        for (let w = 0; w < 15; w++) {
+            tally = tally + this.totalCookies[w];
         }
-    }    
-};
-console.log('Number of customers at PDX Airport: ' + airport.customerGen(), 'Number of cookies purchased: ' + airport.customerPurchase());
-
-airport.totalCookie();
-console.log(airport.total);
-
+        this.totalCookies.push(tally);
+    },        
+    addList: function() {
+        for (let x = 0; x < this.totalCookies.length; x++) {       
+            const list = document.getElementById('airport');
+            const firstLi = document.createElement('li');
+            firstLi.textContent = storeHours[x] + this.totalCookies[x] + ' cookies';
+            list.appendChild(firstLi);
+        }             
+    }
+}    
+airport.customerGen();
+airport.customerPurchase();
+airport.cookieBought();
+airport.cookieTotal();
 airport.addList();
-console.log();
-
-const sum = airport.total.reduce(function (a, b) {
-    return a + b;
-}, 0);
-console.log(sum);
-
+console.log('Number of customers at PDX Airport: ' + airport.customerGen(), 'Number of cookies purchased: ' + airport.customerPurchase());
 
 // Amount of cookies needed at Pionner Square
 const pioneerSquare = {
     minHourly: 3,
     maxHourly: 24,
     cookiePer: 1.2,
+    totalCookies: [],
     customerGen: function() {
-        return Math.floor(this.minHourly) + Math.floor(Math.random() * 24) + 1;
+        return Math.floor(Math.random() * (this.maxHourly - this.minHourly)) + this.minHourly;
     },
     customerPurchase: function() {
-        return Math.floor(Math.random() * 1.2) + 1 + pioneerSquare.customerGen();
+        return Math.floor(Math.random() * this.cookiePer) + 1 + this.customerGen();
     },
-    total: []
-};
-console.log('Number of customers at Pionner Square: ' + pioneerSquare.customerGen(), 'Number of cookies purchased: ' + pioneerSquare.customerPurchase());
-
-function totalCookie1() {
-    for (let i = 0; i < 15; i++) {
-        pioneerSquare.total.push(pioneerSquare.customerPurchase());
-    }
-};
-
-totalCookie1();
-console.log(pioneerSquare.total);
-
-const sum1 = pioneerSquare.total.reduce(function (a, b) {
-    return a + b;
-}, 0);
-console.log(sum1);
-
-// Amount of cookies needed at Powell's
-const powell = {
-    minHourly: 11,
-    maxHourly: 38,
-    cookiePer: 3.7,
-    customerGen: function() {
-        return Math.floor(this.minHourly) + Math.floor(Math.random() * 38) + 1;
-    },
-    customerPurchase: function() {
-        return Math.floor(Math.random() * 3.7) + 1 + powell.customerGen();
-    },
-    total: []
-};
-console.log('Number of customers at Powell\'s: ' + powell.customerGen(), 'Number of cookies purchased: ' + powell.customerPurchase());
-
-function totalCookie2() {
-    for (let i = 0; i < 15; i++) {
-         powell.total.push(powell.customerPurchase());
-        }
-    };
     
-    totalCookie2();
-    console.log(powell.total);
-
-const sum2 = powell.total.reduce(function (a, b) {
-        return a + b;
-    }, 0);
-console.log(sum2);
+    cookieBought: function() {
+        for (let i = 0; i < 15; i++) {
+            this.totalCookies.push(this.customerPurchase());
+        }
+    },
+    cookieTotal: function() {
+        let tally = 0
+        for (let w = 0; w < 15; w++) {
+            tally = tally + this.totalCookies[w];
+        }
+        this.totalCookies.push(tally);
+    },        
+    addList: function() {
+        for (let x = 0; x < this.totalCookies.length; x++) {       
+            const list = document.getElementById('pioneer');
+            const firstLi = document.createElement('li');
+            firstLi.textContent = storeHours[x] + this.totalCookies[x] + ' cookies';
+            list.appendChild(firstLi);
+        } 
+    }
+}        
+pioneerSquare.customerGen();
+pioneerSquare.customerPurchase();
+pioneerSquare.cookieBought();
+pioneerSquare.cookieTotal();
+pioneerSquare.addList();
+console.log('Number of customers at Pionner Square: ' + pioneerSquare.customerGen(), 'Number of cookies purchased: ' + pioneerSquare.customerPurchase());
+    
+// Amount of cookies needed at Powell's
+const powells = {
+    minHourly: 23,
+    maxHourly: 65,
+    cookiePer: 6.3,
+    totalCookies: [],
+    customerGen: function() {
+        return Math.floor(Math.random() * (this.maxHourly - this.minHourly)) + this.minHourly;
+    },
+    customerPurchase: function() {
+        return Math.floor(Math.random() * this.cookiePer) + 1 + this.customerGen();
+    },
+    cookieBought: function() {
+        for (let i = 0; i < 15; i++) { 
+            this.totalCookies.push(this.customerPurchase());
+        }
+    },
+    cookieTotal: function() {
+        let tally = 0
+        for (let w = 0; w < 15; w++) {
+            tally = tally + this.totalCookies[w];
+        }
+        this.totalCookies.push(tally);
+    },        
+    addList: function() {
+        for (let x = 0; x < this.totalCookies.length; x++) {       
+            const list = document.getElementById('powell');
+            const firstLi = document.createElement('li');
+            firstLi.textContent = storeHours[x] + this.totalCookies[x] + ' cookies';
+            list.appendChild(firstLi);
+        }             
+    }
+}    
+powells.customerGen();
+powells.customerPurchase();
+powells.cookieBought();
+powells.cookieTotal();
+powells.addList();
+console.log('Number of customers at Powell\'s: ' + powells.customerGen(), 'Number of cookies purchased: ' + powells.customerPurchase());
 
 // Amount of cookies needed for St. Johns
 const john = {
-    minHourly: 20,
-    maxHourly: 38,
-    cookiePer: 2.3,
+    minHourly: 23,
+    maxHourly: 65,
+    cookiePer: 6.3,
+    totalCookies: [],
     customerGen: function() {
-        return Math.floor(this.minHourly) + Math.floor(Math.random() * 38) + 1;
+        return Math.floor(Math.random() * (this.maxHourly - this.minHourly)) + this.minHourly;
     },
     customerPurchase: function() {
-        return Math.floor(Math.random() * 2.3) + 1 + john.customerGen();
+        return Math.floor(Math.random() * this.cookiePer) + 1 + this.customerGen();
     },
-    total: []
-};
-console.log('Number of customers at St.John: ' + john.customerGen(), 'Number of cookies purchased: ' + john.customerPurchase());
-
-function totalCookie3() {
-    for (let i = 0; i < 15; i++) {
-         john.total.push(john.customerPurchase());
+    cookieBought: function() {
+        for (let i = 0; i < 15; i++) { 
+            this.totalCookies.push(this.customerPurchase());
         }
-    };
-    
-totalCookie3();
-console.log(john.total);
-
-const sum3 = john.total.reduce(function (a, b) {
-        return a + b;
-    }, 0);
-console.log(sum3);
+    },
+    cookieTotal: function() {
+        let tally = 0
+        for (let w = 0; w < 15; w++) {
+            tally = tally + this.totalCookies[w];
+        }
+        this.totalCookies.push(tally);
+    },        
+    addList: function() {
+        for (let x = 0; x < this.totalCookies.length; x++) {       
+            const list = document.getElementById('john');
+            const firstLi = document.createElement('li');
+            firstLi.textContent = storeHours[x] + this.totalCookies[x] + ' cookies';
+            list.appendChild(firstLi);
+        }             
+    }
+}    
+john.customerGen();
+john.customerPurchase();
+john.cookieBought();
+john.cookieTotal();
+john.addList();
+console.log('Number of customers at St. Johns: ' + john.customerGen(), 'Number of cookies purchased: ' + john.customerPurchase());
 
 // Amount needed for the waterfront
 const waterfront = {
-    minHourly: 2,
-    maxHourly: 16,
-    cookiePer: 4.6,
+    minHourly: 23,
+    maxHourly: 65,
+    cookiePer: 6.3,
+    totalCookies: [],
     customerGen: function() {
-        return Math.floor(this.minHourly) + Math.floor(Math.random() * 16) + 1;
+        return Math.floor(Math.random() * (this.maxHourly - this.minHourly)) + this.minHourly;
     },
     customerPurchase: function() {
-        return Math.floor(Math.random() * 4.6) + 1 + waterfront.customerGen();
+        return Math.floor(Math.random() * this.cookiePer) + 1 + this.customerGen();
     },
-    total: []
-};
-console.log('Number of customers at the Waterfront: ' + waterfront.customerGen(), 'Number of cookies purchased: ' + waterfront.customerPurchase());
-
-function totalCookie4() {
-    for (let i = 0; i < 15; i++) {
-         waterfront.total.push(waterfront.customerPurchase());
+    cookieBought: function() {
+        for (let i = 0; i < 15; i++) { 
+            this.totalCookies.push(this.customerPurchase());
         }
-    };
-    
-totalCookie4();
-console.log(waterfront.total);
-
-const sum4 = waterfront.total.reduce(function (a, b) {
-        return a + b;
-    }, 0);
-console.log(sum4);
+    },
+    cookieTotal: function() {
+        let tally = 0
+        for (let w = 0; w < 15; w++) {
+            tally = tally + this.totalCookies[w];
+        }
+        this.totalCookies.push(tally);
+    },        
+    addList: function() {
+        for (let x = 0; x < this.totalCookies.length; x++) {       
+            const list = document.getElementById('waterfront');
+            const firstLi = document.createElement('li');
+            firstLi.textContent = storeHours[x] + this.totalCookies[x] + ' cookies';
+            list.appendChild(firstLi);
+        }             
+    }
+}    
+waterfront.customerGen();
+waterfront.customerPurchase();
+waterfront.cookieBought();
+waterfront.cookieTotal();
+waterfront.addList();
+console.log('Number of customers at The Waterfront: ' + waterfront.customerGen(), 'Number of cookies purchased: ' + waterfront.customerPurchase());
